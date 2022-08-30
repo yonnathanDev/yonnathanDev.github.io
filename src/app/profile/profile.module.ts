6acbaff7739f0d342f3a7499134e14cc2f72c78c
YonnathanDev <yonnathan.placencia.dev>
2022-08-29 22:11:18 -0400

    Create component Porfile: /curriculumVitae, /contact, /portafolio

--- src/app/profile/profile.module.ts
+++ src/app/profile/profile.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CurriculumVitaeComponent } from './pages/curriculum-vitae/curriculum-vitae.component';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { ContactComponent } from './pages/contact/contact.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CurriculumVitaeComponent,
+    PortafolioComponent,
+    ContactComponent
+  ],
   imports: [
     CommonModule
   ]
